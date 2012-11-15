2175fba836a1bc050b1accf8f8ed3203fed1b5f5
Hekar <hekark@gmail.com>
2012-11-15 14:38:02 -0500

    added tests and beginnings of Purchase Totaller

---
+++ Purchase-Totaller/tests/Connection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests
+{
+    [TestClass]
+    public class Connection
+    {
+        [TestMethod]
+        public void TestPublish()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestRegister()
+        {
+        }
+    }
+}
